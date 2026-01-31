7169badb55328d2d8a4988fe3da9cbc4fe87676f
Kitikieres <palmerjuan1670@gmail.com>
2026-01-31 16:55:26 +0100

    aaa

--- GameJam/Assets/GameJamRoot/Scripts/Player/PlayerHit.cs
+++ GameJam/Assets/GameJamRoot/Scripts/Player/PlayerHit.cs
@@ -2,17 +2,28 @@ using UnityEngine;
 
 public class Spikes2D : MonoBehaviour
 {
+    public int damage = 100;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("El jugador murió");
+            Debug.Log("TOCÓ SPIKES");
 
-            
-            other.gameObject.SetActive(false);
+            // BUSCA EL SCRIPT EN EL PADRE
+            PlayerRespawn2D player = other.GetComponentInParent<PlayerRespawn2D>();
 
-         
+            if (player != null)
+            {
+                Debug.Log("APLICANDO DAÑO");
+                player.TakeDamage(damage);
+            }
+            else
+            {
+                Debug.Log("NO SE ENCONTRÓ PlayerRespawn2D EN EL PADRE");
+            }
         }
     }
 }
+
 
