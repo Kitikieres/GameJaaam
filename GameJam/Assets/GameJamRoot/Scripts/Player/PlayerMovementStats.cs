24e17b2716e46da9c8bf7bb7f5a15e67ff3aca96
gaelfofue <gaelfofue2006@gmail.com>
2026-01-31 01:06:55 +0100

    Sigo sin saber que hago

--- GameJam/Assets/GameJamRoot/Scripts/Player/PlayerMovementStats.cs
+++ GameJam/Assets/GameJamRoot/Scripts/Player/PlayerMovementStats.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
 
 [CreateAssetMenu(menuName ="PlayerMovement")]
@@ -21,5 +22,45 @@ public class PlayerMovementStats : ScriptableObject
     public float GroundDetectionRayLength = 0.02f;
     public float HeadDetectionRayLength = 0.3f;
     [Range(0f,1f)] public float HeadWidht = 0.75f;
+
+    [Header("Jump")]
+    public float JumpHeight = 6.5f;
+    [Range(0.1f,2f)] public float JumpHeightCompensationFactor = 1.054f;
+    public float MaxFallSpeed = 26f;
+    [Range(1,5)] public int NumberOfJumpsAllowed = 2;
+
+    [Header("Jump Cut")]
+    [Range(0.02f, 0.3f)] public float TimeForUpwardsCancel = 0.027f;
+
+    [Header("Jump Buffer")]
+    [Range(0f, 1f)] public float JumpBufferTime = 0.125f;
+
+    [Header("Jump Coyote Time")]
+    [Range(0f, 1f)] public float JumpCoyoteTime = 0.1f;
+
+    [Header("Debug")]
+    public bool DebugShowIsGroundedBox;
+    public bool DebugShowHeadBumpBox;
+
+    [Header("JumpVisualization Tool")]
+    public bool ShowWalkJumpArc = false;
+    public bool ShowRunJumpArc = false;
+    public bool StopOnCollision = true;
+    public bool DrawRight = true;
+    [Range(5,100)] public int ArcResolution = 20;
+    [Range(0, 500)] public int VisualizationSteps = 90;
+
+    public float Gravity { get; private set; }
+    public float InitialJumpVelocity { get; private set; }
+
+    private void OnValidate()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+       
+    }
 
 }
